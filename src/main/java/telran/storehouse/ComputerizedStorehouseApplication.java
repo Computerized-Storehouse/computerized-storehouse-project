4d2ff9cdd9ac136d8d1bd3d475dc9d1bddb0f900
AlGuron <blizgur8@gmail.com>
2024-05-24 11:55:04 +0300

    Init project pom

---
+++ src/main/java/telran/storehouse/ComputerizedStorehouseApplication.java
@@ -0,0 +1,13 @@
+package telran.storehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerizedStorehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerizedStorehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/storehouse/ComputerizedStorehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.storehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerizedStorehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
