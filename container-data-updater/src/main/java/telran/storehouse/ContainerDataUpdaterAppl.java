7da39e0e90b5b6a5cfe13c5f12aab08d4640bbd7
AlGuron <blizgur8@gmail.com>
2024-06-02 19:11:03 +0300

    add: spring-cloud-stream consumers

--- container-data-updater/src/main/java/telran/storehouse/ContainerDataUpdaterAppl.java
+++ container-data-updater/src/main/java/telran/storehouse/ContainerDataUpdaterAppl.java
@@ -1,9 +1,33 @@
 package telran.storehouse;
 
-import org.springframework.boot.SpringApplication;
+import java.util.function.Consumer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+
+import telran.storehouse.dto.ContainerDataDto;
+import telran.storehouse.dto.NewStateDto;
+import telran.storehouse.service.ContainerDataUpdaterService;
+
+@SpringBootApplication
+@RequiredArgsConstructor
 public class ContainerDataUpdaterAppl {
+	final ContainerDataUpdaterService service;
+
 	public static void main(String args[]) {
 		SpringApplication.run(ContainerDataUpdaterAppl.class, args);
+	}
+
+	@Bean
+	Consumer<NewStateDto> containerFullnessUpdateConsumer() {
+		return service::updateContanerCurrentFullness;
+	}
+
+	@Bean
+	Consumer<ContainerDataDto> containerStatusUpdateConsumer() {
+		return service::updateContanerStatus;
 	}
 }
